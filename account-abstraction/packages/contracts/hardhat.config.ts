8a97cc5477b54a6f0685d5be6263547019903012
taijusanagi <taijusanagi@gmail.com>
2022-12-03 13:15:12 +0530

    account-abstraction-starter

---
+++ account-abstraction/packages/contracts/hardhat.config.ts
@@ -0,0 +1,64 @@
+import "@nomicfoundation/hardhat-toolbox";
+
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+
+import { HARDHAT_CHAINID, TIMEOUT } from "./config";
+import { getMnemonic } from "./lib/dev/mnemonic";
+import { getNetworksUserConfigs } from "./lib/dev/network";
+import networkJsonFile from "./network.json";
+import { ChainId } from "./types/network";
+
+dotenv.config();
+
+const mnemonic = getMnemonic("../../mnemonic.txt");
+const networksUserConfigs = getNetworksUserConfigs(mnemonic);
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.15",
+      },
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat:
+      process.env.IS_INTEGRATION_TEST === "true"
+        ? {
+            chainId: Number(process.env.FORK_CHAIN_ID),
+            accounts: {
+              mnemonic,
+            },
+            forking: {
+              url: networkJsonFile[process.env.FORK_CHAIN_ID as ChainId].rpc,
+            },
+          }
+        : {
+            chainId: HARDHAT_CHAINID,
+            accounts: {
+              mnemonic,
+            },
+          },
+    ...networksUserConfigs,
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API || "",
+    },
+  },
+  gasReporter: {
+    enabled: process.env.IS_GAS_REPORTER_ENABLED === "true",
+  },
+  mocha: {
+    timeout: TIMEOUT,
+  },
+};
+
+export default config;
