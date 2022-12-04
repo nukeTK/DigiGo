9d088f3394ef8dc2a824d950791a538d63c2be61
taijusanagi <taijusanagi@gmail.com>
2022-12-04 07:57:16 +0530

    done

--- packages/app/src/pages/experimental/account-abstraction.tsx
+++ packages/app/src/pages/experimental/account-abstraction.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable camelcase */
-import { Button, Stack, Text } from "@chakra-ui/react";
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { Button, Input, Stack, Text } from "@chakra-ui/react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import WalletConnect from "@walletconnect/client";
+import { convertHexToUtf8 } from "@walletconnect/utils";
 import { NextPage } from "next";
 import { useState } from "react";
 import { useAccount, useSigner } from "wagmi";
@@ -15,7 +17,7 @@ import { MockPayment__factory } from "../../../../../account-abstraction/package
 import configJsonFile from "../../../config.json";
 
 const AccountAbstractionPage: NextPage = () => {
-  const { address } = useAccount();
+  const { address, isConnected } = useAccount();
   const { data: signer } = useSigner();
   const { handle } = useErrorToast();
 
@@ -23,41 +25,124 @@ const AccountAbstractionPage: NextPage = () => {
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const deploySafe = async () => {
-    if (!address || !signer || !signer.provider || !digiGoWallet) {
+  const [walletConnectURI, setWalletConnectURI] = useState("");
+  const [isWalletConnectConnecting, setIsWalletConnectConnecting] = useState(false);
+  const [isWalletConnectSessionEstablished, setIsWalletConnectSessionEstablished] = useState(false);
+  const [connectedApp, setConnectedApp] = useState<{ name: string; url: string }>();
+
+  const connectWithWalletConnect = async (walletConnectURI: string) => {
+    if (!signer || !digiGoWallet) {
       return;
     }
+    setIsWalletConnectConnecting(true);
     try {
-      const mockPayment = MockPayment__factory.connect(deploymentsJsonFile.mockPayment, signer);
-      console.log(digiGoWallet.address);
-
-      // await signer.sendTransaction({ to: digiGoWallet.address, value: ethers.utils.parseEther("0.5") });
-      const data = mockPayment.interface.encodeFunctionData("pay");
-      const op = await digiGoWallet.userOpHandler.createSignedUserOp({
-        target: mockPayment.address,
-        data,
-        value: 100,
-        gasLimit: 6100000,
+      let walletConnectConnector = new WalletConnect({
+        uri: walletConnectURI,
       });
-      const tx = await digiGoWallet.bundlerClient.sendUserOpToBundler(op);
-      console.log(tx);
-      setIsLoading(true);
-      console.log(digiGoWallet);
+      if (walletConnectConnector.connected) {
+        console.log("kill previous session and recreate session");
+
+        await walletConnectConnector.killSession();
+        walletConnectConnector = new WalletConnect({
+          uri: walletConnectURI,
+        });
+      }
+      walletConnectConnector.on("session_request", async (error, payload) => {
+        console.log("session_request", payload);
+        if (error) {
+          throw error;
+        }
+        console.log("approving session");
+        walletConnectConnector.approveSession({ chainId: 80001, accounts: [digiGoWallet.address] });
+        console.log("session approved");
+        const { peerMeta } = payload.params[0];
+        setConnectedApp({ ...peerMeta });
+        setIsWalletConnectConnecting(false);
+        setIsWalletConnectSessionEstablished(true);
+      });
+      walletConnectConnector.on("call_request", async (error, payload) => {
+        console.log("call_request", payload);
+        if (error) {
+          throw error;
+        }
+        if (payload.method === "eth_sendTransaction") {
+          console.log("eth_sendTransaction");
+          // await processTx(
+          //   linkWalletAddress,
+          //   payload.params[0].to,
+          //   payload.params[0].data,
+          //   payload.params[0].value,
+          //   payload.params[0].gas
+          // );
+          // walletConnectConnector.approveRequest({
+          //   id: payload.id,
+          //   result: transactionHash,
+          // });
+        }
+        if (payload.method === "personal_sign") {
+          console.log("personal_sign");
+          const message = convertHexToUtf8(payload.params[0]);
+          console.log("signing message");
+          const signature = await signer.signMessage(message);
+          console.log("signature", signature);
+          walletConnectConnector.approveRequest({
+            id: payload.id,
+            result: signature,
+          });
+        }
+        if (payload.method === "eth_signTypedData") {
+          console.log("eth_signTypedData");
+          // console.log("signing message");
+          // console.log(payload.params[1]);
+          // const { domain, message: value, types } = JSON.parse(payload.params[1]);
+          // delete types.EIP712Domain;
+          // console.log(domain, types, value);
+          // const signature = await signTypedData({ domain, types, value });
+          // console.log("signature", signature);
+          // walletConnectConnector.approveRequest({
+          //   id: payload.id,
+          //   result: signature,
+          // });
+        }
+      });
+      walletConnectConnector.on("disconnect", (error, payload) => {
+        console.log("disconnect", payload);
+        if (error) {
+          throw error;
+        }
+        // clearWalletConnect();
+      });
     } catch (e) {
       handle(e);
-    } finally {
-      setIsLoading(false);
+      // clearWalletConnect();
     }
   };
 
   return (
     <Layout>
       <Unit header="Account Abstraction Identity Portal">
-        <Stack>
+        <Stack spacing="4">
           <Text fontSize="sm" color={configJsonFile.style.color.black.text.secondary}>
             Here you can enable Account Abstraction with Safe
           </Text>
           <ConnectButton />
+          {isConnected && digiGoWallet && (
+            <Stack>
+              <Text fontSize={"sm"} fontWeight={"bold"}>
+                Account
+              </Text>
+              <Text fontSize={"sm"}>{digiGoWallet.address}</Text>
+              <Text fontSize={"sm"} fontWeight={"bold"}>
+                Wallet Connect
+              </Text>
+              <Input
+                placeholder="wallet connect"
+                onChange={(e) => setWalletConnectURI(e.target.value)}
+                value={walletConnectURI}
+              ></Input>
+              <Button onClick={() => connectWithWalletConnect(walletConnectURI)}>Connect</Button>
+            </Stack>
+          )}
         </Stack>
       </Unit>
     </Layout>
